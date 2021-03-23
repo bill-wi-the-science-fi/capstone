04ce56eaccbbe960e1e7764f7898542c336488b5
Viral Patel <vpatel621@gmail.com>
2021-03-23 16:13:46 -0400

    Feature: email logic updated

--- server/api/nominate.js
+++ server/api/nominate.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 const {User, Nomination, Award} = require('../db/models')
+const sendEmail = require('../email/email')
+
 module.exports = router
 
 // nominateUser thunk
@@ -45,6 +47,8 @@ router.post('/', async (req, res, next) => {
     // Add first and last name to the nominee & set address
     if (userWasCreated) {
       await nominee.update({firstName: firstName, lastName: lastName})
+      //placeholder url until we create an identifier
+      sendEmail(email, firstName, nominator.firstName)
       recipientAddress = nominator.ethPublicAddress
     } else recipientAddress = nominee.ethPublicAddress
 
@@ -58,3 +62,34 @@ router.post('/', async (req, res, next) => {
     next(err)
   }
 })
+
+//in the database we want a hook
+
+//hook should be after validation
+
+//math.random 6 digits
+
+//user.update with math.random value
+
+//await that entry into database
+
+//if that passes, then send the email with pin.....
+
+//user reads email- clicks to signup
+
+//user should sign up using a special link /signup/nominee (front end)
+//backend route = users/nominee
+
+//enters pin on form
+
+//when the user hits the post route (generic), we need to check if user previously existed or not
+
+//if user existed, and doesnt provide a pin send error (nominee) ask to sign up with pin
+
+//if user did not exist but tries to provide a pin send a error
+
+//if user existed, and did provide a pin and its verified-great (nominee)
+
+//if user didnt exist, and didnt provide a pin-great
+
+//
