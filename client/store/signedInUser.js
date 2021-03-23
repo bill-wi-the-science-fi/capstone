0e20612c91d8d596040a643c4abafc93d0fa2a87
Alpay Aldemir <aldemir.alpay@gmail.com>
2021-03-23 16:54:08 -0400

    Feat: On sign up didMount, user is prompted to conect meta mask, if user declines or does not have MM will be re-routed back home

--- client/store/signedInUser.js
+++ client/store/signedInUser.js
@@ -31,27 +31,45 @@ export const me = () => async (dispatch) => {
   }
 }
 
-export const auth = (
-  firstName,
-  lastName,
-  email,
-  password,
-  imgUrl,
-  method
-) => async (dispatch) => {
-  let res
-  try {
-    res = await axios.post(`/auth/${method}`, {
+export function authSignUp(userInfoObject, method) {
+  return async (dispatch) => {
+    const {
+      ethPublicAddress,
       firstName,
       lastName,
       email,
       password,
       imgUrl
-    })
+    } = userInfoObject
+    let res
+    try {
+      res = await axios.post(`/auth/${method}`, {
+        ethPublicAddress,
+        firstName,
+        lastName,
+        email,
+        password,
+        imgUrl
+      })
+    } catch (authError) {
+      return dispatch(getUser({error: authError}))
+    }
+
+    try {
+      dispatch(getUser(res.data))
+      history.push('/home')
+    } catch (dispatchOrHistoryErr) {
+      console.error(dispatchOrHistoryErr)
+    }
+  }
+}
+export const auth = (email, password, method) => async (dispatch) => {
+  let res
+  try {
+    res = await axios.post(`/auth/${method}`, {email, password})
   } catch (authError) {
     return dispatch(getUser({error: authError}))
   }
-
   try {
     dispatch(getUser(res.data))
     history.push('/home')
