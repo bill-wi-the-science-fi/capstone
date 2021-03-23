3667e5945be71a5d65480bd53114248b71a0f275
Viral Patel <vpatel621@gmail.com>
2021-03-23 15:12:10 -0400

    Feat: email template added to email request

--- server/email/email.js
+++ server/email/email.js
@@ -2,31 +2,43 @@ const sgMail = require('@sendgrid/mail')
 const axios = require('axios')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-const data = JSON.stringify({
-  personalizations: [
-    {
-      to: [{email: 'vpatel2@binghamton.edu', name: 'John Doe'}],
-      subject: 'Hello, World!'
-    }
-  ],
-  from: {email: 'vpatel621@gmail.com', name: 'John Doe'},
-  reply_to: {email: 'vpatel621@gmail.com', name: 'John Doe'},
-  content: [{value: 'Working Hello from the other side', type: 'text/plain'}]
-})
-const config = {
-  method: 'post',
-  url: 'https://api.sendgrid.com/v3/mail/send',
-  headers: {
-    Authorization:
-      'Bearer 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',
-    'Content-Type': 'application/json'
-  },
-  data: data
+function sendEmail(recipientEmail, firstName, nominatorName, recipientUrl) {
+  const data = JSON.stringify({
+    personalizations: [
+      {
+        // to: [{email: 'vpatel2@binghamton.edu', name: 'John Doe'}],
+
+        to: [{email: recipientEmail, name: firstName, recipientUrl}],
+        subject: `You've been nominated by: ${nominatorName} to win an award`
+      }
+    ],
+    from: {email: 'vpatel621@gmail.com', name: 'Unicorn Corp'},
+
+    reply_to: {email: 'vpatel621@gmail.com', name: 'Unicorn Corp'},
+    content: [
+      {
+        value: `Hi:${firstName},
+                You have been nominated to receive an award. Please sign up using the link below to receive
+                your award!
+                ${recipientUrl}`,
+        type: 'text/plain'
+      }
+    ]
+  })
+  const config = {
+    method: 'post',
+    url: 'https://api.sendgrid.com/v3/mail/send',
+    headers: {
+      Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
+      'Content-Type': 'application/json'
+    },
+    data: data
+  }
+  axios(config)
+    .then(function (response) {
+      console.log(response.config)
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
 }
-axios(config)
-  .then(function (response) {
-    console.log(response.config)
-  })
-  .catch(function (error) {
-    console.log(error)
-  })
