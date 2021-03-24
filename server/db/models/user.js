82a95f0cb8843e18366204a13561b7010e812809
ATunaMelt <76219533+ATunaMelt@users.noreply.github.com>
2021-03-24 12:06:41 -0400

    Fix: Fixed salt hashing (#16)

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -26,12 +26,12 @@ const User = db.define('user', {
       return () => this.getDataValue('password')
     }
   },
-  signUpPin: {
+  pin: {
     type: Sequelize.STRING,
-    // Making `.password` act like a func hides it when serializing to JSON.
+    // Making `.pin` act like a func hides it when serializing to JSON.
     // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
-      return () => this.getDataValue('signUpPin')
+      return () => this.getDataValue('pin')
     }
   },
   salt: {
@@ -40,6 +40,14 @@ const User = db.define('user', {
     // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
       return () => this.getDataValue('salt')
+    }
+  },
+  saltpin: {
+    type: Sequelize.STRING,
+    // Making `.saltpin` act like a function hides it when serializing to JSON.
+    // This is a hack to get around Sequelize's lack of a "private" option.
+    get() {
+      return () => this.getDataValue('saltpin')
     }
   },
   googleId: {
@@ -67,14 +75,17 @@ User.prototype.correctPassword = function (candidatePwd) {
   return User.encryptPassword(candidatePwd, this.salt()) === this.password()
 }
 
-User.prototype.correctSignUpPin = function (candidatePin) {
-  return User.encryptSignUpPin(candidatePin, this.salt()) === this.signUpPin()
+User.prototype.correctPin = function (candidatePin) {
+  return User.encryptPin(candidatePin, this.saltpin()) === this.pin()
 }
 
 /**
  * classMethods
  */
 User.generateSalt = function () {
+  return crypto.randomBytes(16).toString('base64')
+}
+User.generatePinSalt = function () {
   return crypto.randomBytes(16).toString('base64')
 }
 
@@ -85,11 +96,11 @@ User.encryptPassword = function (plainText, salt) {
     .update(salt)
     .digest('hex')
 }
-User.encryptSignUpPin = function (plainText, salt) {
+User.encryptPin = function (plainText, saltpin) {
   return crypto
     .createHash('RSA-SHA256')
     .update(plainText)
-    .update(salt)
+    .update(saltpin)
     .digest('hex')
 }
 
@@ -102,11 +113,10 @@ const setSaltAndPassword = (user) => {
     user.password = User.encryptPassword(user.password(), user.salt())
   }
 }
-const setSignUpPin = (user) => {
-  if (user.changed('signUpPin')) {
-    user.salt = User.generateSalt()
-
-    user.signUpPin = User.encryptSignUpPin(user.signUpPin(), user.salt())
+const setsaltpinAndPin = (user) => {
+  if (user.changed('pin')) {
+    user.saltpin = User.generatePinSalt()
+    user.pin = User.encryptPin(user.pin(), user.saltpin())
   }
 }
 
@@ -115,8 +125,8 @@ User.beforeUpdate(setSaltAndPassword)
 User.beforeBulkCreate((users) => {
   users.forEach(setSaltAndPassword)
 })
-User.beforeCreate(setSignUpPin)
-User.beforeUpdate(setSignUpPin)
+User.beforeCreate(setsaltpinAndPin)
+User.beforeUpdate(setsaltpinAndPin)
 User.beforeBulkCreate((users) => {
-  users.forEach(setSignUpPin)
+  users.forEach(setsaltpinAndPin)
 })
