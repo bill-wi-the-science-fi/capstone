bb9712fd7aaed52e7a27c1194fda7e0f6d55c992
Viral Patel <vpatel621@gmail.com>
2021-03-27 18:59:33 -0400

    Feat:Pagination added

--- client/components/AllAwards.js
+++ client/components/AllAwards.js
@@ -3,18 +3,59 @@ import {Button, Card} from 'react-bootstrap'
 import {connect} from 'react-redux'
 import {getAllAwards} from '../store'
 import {Link} from 'react-router-dom'
+import ReactPaginate from 'react-paginate'
 
 /**
  * COMPONENT
  */
 
 class AllAwards extends Component {
-  componentDidMount() {
-    this.props.getAllAwards()
+  constructor() {
+    super()
+    this.state = {
+      offset: 0,
+      awards: [],
+      perPage: 4,
+      currentPage: 0
+    }
+    this.pagination = this.pagination.bind(this)
+    this.handlePageClick = this.handlePageClick.bind(this)
+  }
+
+  pagination() {
+    //data for awards to display on page
+    const awards = this.props.awards.slice(
+      this.state.offset,
+      this.state.offset + this.state.perPage
+    )
+
+    this.setState((state) => ({
+      awards,
+      pageCount: Math.ceil(this.props.awards.length / state.perPage)
+    }))
+  }
+  handlePageClick = (e) => {
+    const selectedPage = e.selected
+    const offset = selectedPage * this.state.perPage
+
+    this.setState(
+      {
+        currentPage: selectedPage,
+        offset: offset
+      },
+      () => {
+        this.pagination()
+      }
+    )
+  }
+
+  async componentDidMount() {
+    await this.props.getAllAwards()
+    this.pagination()
   }
 
   render() {
-    const {awards} = this.props
+    const {awards} = this.state
 
     if (!awards.length) {
       return <h2> Loading awards... </h2>
@@ -43,6 +84,21 @@ class AllAwards extends Component {
               </Card>
             </div>
           ))}
+        </div>
+        <div className="centered">
+          <ReactPaginate
+            previousLabel="prev"
+            nextLabel="next"
+            breakLabel="..."
+            breakClassName="break-me"
+            pageCount={this.state.pageCount}
+            marginPagesDisplayed={1}
+            pageRangeDisplayed={1}
+            onPageChange={this.handlePageClick}
+            containerClassName="pagination"
+            subContainerClassName="pages pagination"
+            activeClassName="active"
+          />
         </div>
       </div>
     )
