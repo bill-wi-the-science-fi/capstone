00930d0b9a1401164460e94bee7253bfe49a0835
Alan Watson <alanowatson@gmail.com>
2021-04-01 15:23:34 -0400

    Feature: Image stretch issues fixed, share buttons styled

--- client/components/ShareButton.js
+++ client/components/ShareButton.js
@@ -17,22 +17,22 @@ export const ShareButton = (props) => {
   return (
     <div>
       {' '}
-      <h6>Spread the Word!</h6>
+      <h6 className="m-1">Spread the Word!</h6>
       <div>
-        <FacebookShareButton url={props.url} quote="hi">
-          <FacebookIcon size="2.5rem" />
+        <FacebookShareButton url={props.url} quote="Check out this award!">
+          <FacebookIcon className="rounded m-1" size="2.5rem" />
         </FacebookShareButton>
-        <TwitterShareButton url={props.url} quote="hi">
-          <TwitterIcon size="2.5rem" />
+        <TwitterShareButton url={props.url} quote="Check out this award!">
+          <TwitterIcon className="rounded m-1" size="2.5rem" />
         </TwitterShareButton>
-        <LinkedinShareButton url={props.url} quote="hi">
-          <LinkedinIcon size="2.5rem" />
+        <LinkedinShareButton url={props.url} quote="Check out this award!">
+          <LinkedinIcon className="rounded m-1" size="2.5rem" />
         </LinkedinShareButton>
-        <WhatsappShareButton url={props.url} quote="hi">
-          <WhatsappIcon size="2.5rem" />
+        <WhatsappShareButton url={props.url} quote="Check out this award!">
+          <WhatsappIcon className="rounded m-1" size="2.5rem" />
         </WhatsappShareButton>
-        <RedditShareButton url={props.url} quote="hi">
-          <RedditIcon size="2.5rem" />
+        <RedditShareButton url={props.url} quote="Check out this award!">
+          <RedditIcon className="rounded m-1" size="2.5rem" />
         </RedditShareButton>
       </div>
     </div>
