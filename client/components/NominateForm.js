24b1a9b05da3d3062f32187ea16bbe0d3d723345
ATunaMelt <colekuntzman@protonmail.com>
2021-04-04 16:58:57 -0400

    Fix: deleted consolelog

--- client/components/NominateForm.js
+++ client/components/NominateForm.js
@@ -190,10 +190,6 @@ class NominateForm extends Component {
               // )
               let currentURL = window.location.href;
               currentURL = currentURL.slice(0, currentURL.length - 8);
-              console.log(
-                '\n --------🚀 \n router.post \n currentURL',
-                currentURL
-              );
               formData.currentURL = currentURL;
               await this.props.nominateUser(formData);
               this.startAwardAndDonate(
