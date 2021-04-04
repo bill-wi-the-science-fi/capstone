565f8ae2a966a9dc64b19ef3563a5d9a20fecf5a
ATunaMelt <76219533+ATunaMelt@users.noreply.github.com>
2021-04-04 13:20:05 -0400

    Feat: New deploy script for smart contract and heroku (#72)
    * Feat: New deploy script for smart contract and heroku
    
    * Deploying
    
    * Fix: Added npx
    
    * Fix: Re-checkout current branch NOT master
    
    * Fix: Delete git ignore files

--- server/api/Relayer/relayerToContract.js
+++ server/api/Relayer/relayerToContract.js
@@ -4,7 +4,9 @@ const nominateContractAddress = Nominate.networks[3].address;
 const relayerAddress = '0x7714e9182799ce2f92b26e70c9cd55cd1b3c1d38';
 const Web3 = require('web3');
 const {DefenderRelayProvider} = require('defender-relay-client/lib/web3');
-const {credentials} = require('../../../secrets');
+const RELAYER_KEY = process.env.RELAYER_KEY;
+const RELAYER_SECRET = process.env.RELAYER_SECRET;
+const credentials = {apiKey: RELAYER_KEY, apiSecret: RELAYER_SECRET};
 
 const provider = new DefenderRelayProvider(credentials, {speed: 'fast'});
 const web3 = new Web3(provider);
